7a509d8793a2b91d32a2a4070034b6b6f8630dc9
Stéphane Angel <s.angel@twidi.com>
2014-11-19 15:45:26 +0100

    Add missing comas in setup.py classifiers

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2"
-        "Programming Language :: Python :: 2.7"
-        "Programming Language :: Python :: 3"
-        "Programming Language :: Python :: 3.2"
-        "Programming Language :: Python :: 3.3"
-        "Programming Language :: Python :: 3.4"
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ]
 )
